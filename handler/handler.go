782064138e9a34346ad3ee01b7fb1e0f5b1a89c3
Gerben Jacobs <jacobs.gerben@gmail.com>
2024-11-30 17:19:49 +0100

    rename kindy data url to /kd so I don't lose /photos

--- handler/handler.go
+++ handler/handler.go
@@ -32,23 +32,25 @@ func New(dependencies Dependencies) *Handler {
 
 	r := http.NewServeMux()
 
+	// static
 	r.Handle("GET /images/", http.StripPrefix("/images/", http.FileServer(http.Dir("static/images"))))
 	r.Handle("GET /css/", http.StripPrefix("/css/", http.FileServer(http.Dir("static/css"))))
 	r.HandleFunc("GET /robots.txt", singlePage("static/robots.txt"))
 	r.HandleFunc("GET /humans.txt", singlePage("static/humans.txt"))
 
+	// Pages
 	r.HandleFunc("GET /{$}", h.singlePageLayout("static/views/index.html"))
 	r.HandleFunc("GET /joke", h.singlePageLayout("content/single/joke.html"))
 	r.HandleFunc("GET /changelog", h.singlePageLayout("content/single/changelog.html"))
 	r.HandleFunc("GET /sitemap", h.singlePageLayout("content/single/sitemap.html"))
-
 	r.HandleFunc("GET /tags/{tag}", h.tags)
+
+	// Kindy endpoints
 	r.HandleFunc("GET /notes/{file}", Kindy)
 	r.HandleFunc("GET /posts/{file}", Kindy)
 	r.HandleFunc("GET /likes/{file}", Kindy)
 	r.HandleFunc("GET /replies/{file}", Kindy)
-	r.Handle("GET /photos/", http.StripPrefix("/photos/", http.FileServer(http.Dir("content/kindy/data/photo"))))
-
+	r.Handle("GET /kd/", http.StripPrefix("/kd/", http.FileServer(http.Dir("content/kindy/data"))))
 	r.HandleFunc("/kindy", internal.BasicAuth(kindyEditor, h.SecretKey))
 
 	h.mux = internal.LogWriter(r)
